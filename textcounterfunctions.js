8573f3558644d5695173ea6a573fbc348c031949
SlowO <almogosher2@gmail.com>
2021-06-27 22:14:23 -0700

    Adding HTML and JS files to the repo

---
+++ textcounterfunctions.js
@@ -0,0 +1,56 @@
+let resultObj = {
+    "chars": 0,
+    "words": 0,
+    "alphabet": 0,
+    "digits": 0,
+    "specials": 0,
+    "spaces": 0,
+    "doublespace": 0
+}
+
+function getText() {
+    const value = document.querySelector('textarea').value;
+    resultObj.chars = value.length;
+
+    const words = value.split(' ');
+    resultObj.words = countNoneSpaceChar(words);
+
+    let alphabet = value.match(/[a-z]/gi);
+    resultObj.alphabet = alphabet.length;
+
+    let numeric = value.match(/[0-9]/gi);
+    resultObj.digits = numeric == null ? 0 : numeric.length;
+
+    let noneAlphaNumeric = value.match(/[^\w\*]/g);
+    resultObj.specials = countNoneSpaceChar(noneAlphaNumeric);
+
+    resultObj.spaces = value.match(/\s/g).length;
+
+    let ds = value.match(/\s{2,}/g);
+    resultObj.doublespace = ds == null ? 0 : ds.length;
+
+    let currentValues = document.querySelectorAll('span');
+    Array.from(currentValues).forEach(function(value) {
+        Object.keys(resultObj).forEach(key => {
+            if (key == value.id && resultObj[key] != 0) {
+                value.textContent = resultObj[key];
+            }
+          });
+    });
+};
+
+function countNoneSpaceChar(arr) {
+    let wordCount = 0;
+    arr.forEach(element => {
+        if (element != " " 
+        && element != "" 
+        && element != "\n")
+        {
+            wordCount++
+        }
+    });
+    return wordCount;
+}
+
+
+
